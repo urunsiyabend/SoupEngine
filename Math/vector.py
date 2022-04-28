d4699d30a2a68b3ae9e7a2db9878c80d5ddea152
Siyabend Ürün <urunsiyabend@gmail.com>
2022-04-28 21:21:24 +0300

    Rename vector classes

--- Math/vector.py
+++ Math/vector.py
@@ -1,7 +1,7 @@
 import math
 
 
-class Vector2f:
+class Vector2:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
@@ -10,28 +10,28 @@ class Vector2f:
         return f"<Vector2f> ({self.x:.2f}, {self.y:.2f})"
 
     def __add__(self, other):
-        if isinstance(other, Vector2f):
-            return Vector2f(self.x + other.x, self.y + other.y)
+        if isinstance(other, Vector2):
+            return Vector2(self.x + other.x, self.y + other.y)
         elif isinstance(other, (float, int)):
-            return Vector2f(self.x + other, self.y + other)
+            return Vector2(self.x + other, self.y + other)
 
     def __sub__(self, other):
-        if isinstance(other, Vector2f):
-            return Vector2f(self.x - other.x, self.y - other.y)
+        if isinstance(other, Vector2):
+            return Vector2(self.x - other.x, self.y - other.y)
         elif isinstance(other, (float, int)):
-            return Vector2f(self.x - other, self.y - other)
+            return Vector2(self.x - other, self.y - other)
 
     def __mul__(self, other):
-        if isinstance(other, Vector2f):
-            return Vector2f(self.x * other.x, self.y * other.y)
+        if isinstance(other, Vector2):
+            return Vector2(self.x * other.x, self.y * other.y)
         elif isinstance(other, (float, int)):
-            return Vector2f(self.x * other, self.y * other)
+            return Vector2(self.x * other, self.y * other)
 
     def __div__(self, other):
-        if isinstance(other, Vector2f):
-            return Vector2f(self.x / other.x, self.y / other.y)
+        if isinstance(other, Vector2):
+            return Vector2(self.x / other.x, self.y / other.y)
         elif isinstance(other, (float, int)):
-            return Vector2f(self.x / other, self.y / other)
+            return Vector2(self.x / other, self.y / other)
 
     @property
     def x(self) -> float:
@@ -57,17 +57,17 @@ class Vector2f:
         return self.x * o.x + self.y * o.y
 
     def normalize(self):
-        return Vector2f(self.x / self.length, self.y / self.length)
+        return Vector2(self.x / self.length, self.y / self.length)
 
     def rotate(self, angle: float):
         rad = math.radians(angle)
         cos = math.cos(rad)
         sin = math.sin(rad)
 
-        return Vector2f(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
+        return Vector2(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
 
 
-class Vector3f:
+class Vector3:
     def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
@@ -77,28 +77,28 @@ class Vector3f:
         return f"<Vector3f> ({self.x:.2f}, {self.y:.2f}, {self.z:.2f})"
 
     def __add__(self, other):
-        if isinstance(other, Vector3f):
-            return Vector3f(self.x + other.x, self.y + other.y, self.z + other.z)
+        if isinstance(other, Vector3):
+            return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
         elif isinstance(other, (float, int)):
-            return Vector3f(self.x + other, self.y + other, self.z + other)
+            return Vector3(self.x + other, self.y + other, self.z + other)
 
     def __sub__(self, other):
-        if isinstance(other, Vector3f):
-            return Vector3f(self.x - other.x, self.y - other.y, self.z - other.z)
+        if isinstance(other, Vector3):
+            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
         elif isinstance(other, (float, int)):
-            return Vector3f(self.x - other, self.y - other, self.z - other)
+            return Vector3(self.x - other, self.y - other, self.z - other)
 
     def __mul__(self, other):
-        if isinstance(other, Vector3f):
-            return Vector3f(self.x * other.x, self.y * other.y, self.z * other.z)
+        if isinstance(other, Vector3):
+            return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
         elif isinstance(other, (float, int)):
-            return Vector3f(self.x * other, self.y * other, self.z * other)
+            return Vector3(self.x * other, self.y * other, self.z * other)
 
     def __div__(self, other):
-        if isinstance(other, Vector3f):
-            return Vector3f(self.x / other.x, self.y / other.y, self.z / other.z)
+        if isinstance(other, Vector3):
+            return Vector3(self.x / other.x, self.y / other.y, self.z / other.z)
         elif isinstance(other, (float, int)):
-            return Vector3f(self.x / other, self.y / other, self.z / other)
+            return Vector3(self.x / other, self.y / other, self.z / other)
 
     @property
     def x(self) -> float:
@@ -136,10 +136,10 @@ class Vector3f:
         y_ = self.z * o.x - self.x * o.z
         z_ = self.x * o.y - self.y * o.x
 
-        return Vector3f(x_, y_, z_)
+        return Vector3(x_, y_, z_)
 
     def normalize(self):
-        return Vector3f(self.x / self.length, self.y / self.length, self.z / self.length)
+        return Vector3(self.x / self.length, self.y / self.length, self.z / self.length)
 
     def rotate(self, angle: float):
         # TODO: implement 3D rotation
