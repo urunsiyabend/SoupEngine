35c5446c657ac03ac57fdb6b3d50fb9bcbb321bc
Siyabend Ürün <urunsiyabend@gmail.com>
2022-05-01 12:47:43 +0300

    Set pygame for opengl association

--- window.py
+++ window.py
@@ -1,16 +1,19 @@
 import pygame
+from pygame.locals import *
 
 
 class Window:
     @staticmethod
     def create_window(width: int, height: int, title: str) -> None:
         pygame.init()
-        pygame.display.set_mode([width, height])
+        display = (width, height)
+        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
         pygame.display.set_caption(title)
+
 
     @staticmethod
     def render() -> None:
-        pygame.display.update()
+        pygame.display.flip()
 
     @staticmethod
     def dispose() -> None:
@@ -18,17 +21,17 @@ class Window:
 
     @staticmethod
     def is_closed() -> bool:
-        for event in pygame.event.get(pygame.QUIT):
+        for _ in pygame.event.get(pygame.QUIT):
             return True
 
-    @staticmethod
-    def get_width() -> int:
+    @property
+    def width(self) -> int:
         return pygame.display.get_window_size()[0]
 
-    @staticmethod
-    def get_height() -> int:
+    @property
+    def height(self) -> int:
         return pygame.display.get_window_size()[1]
 
-    @staticmethod
-    def get_title() -> (str, str):
+    @property
+    def title(self) -> (str, str):
         return pygame.display.get_caption()
